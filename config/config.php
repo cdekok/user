7d23848f953b267b4f2523cddc5f3c29da49ed4d
chris <chris.de.kok@gmail.com>
2014-04-28 15:03:01 +0200

    Add structure

---
+++ config/config.php
@@ -0,0 +1,10 @@
+<?php
+return [
+    'routes' => [
+        "/posts" => [
+            'module'     => '\\Cept\\User',
+            'controller' => '\\Cept\User\Controller\\LoginController',
+            'action'     => 'login',
+        ]
+    ]
+];
---
+++ src/Module.php
@@ -0,0 +1,24 @@
+<?php
+namespace Cept\Blog;
+
+class Module implements \Phalcon\Mvc\ModuleDefinitionInterface
+{
+    public function registerAutoloaders() {
+    }
+    
+    public function registerServices($di) {
+        //Registering a dispatcher
+        $di->set('dispatcher', function() {
+            $dispatcher = new \Phalcon\Mvc\Dispatcher();
+            $dispatcher->setDefaultNamespace(__NAMESPACE__."\Controller");
+            return $dispatcher;
+        });
+
+        //Registering the view component
+        $di->set('view', function() {
+            $view = new \Phalcon\Mvc\View();
+            $view->setViewsDir(realpath(__DIR__.'/..//view/'));
+            return $view;
+        });
+    }
+}
